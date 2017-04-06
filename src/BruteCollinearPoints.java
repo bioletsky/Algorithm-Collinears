ab4b0bd34115acc2fbf3b9ff605e7356bb58a628
bioletsky <bioletsky@gmail.com>
2017-04-06 23:37:08 +0300

    сдуру сделал Node

--- src/BruteCollinearPoints.java
+++ src/BruteCollinearPoints.java
@@ -1,10 +1,31 @@
 import java.util.Arrays;
+import java.util.Comparator;
 
 /**
  * Created by obil on 06.04.17.
  */
 public class BruteCollinearPoints {
     // private Point[] points;
+
+    private  int numOfSeg = 0;
+    private Node first = null;
+
+    private class Node {
+        private LineSegment item;
+        private Node next;
+        public Node(LineSegment item, Node next) {
+            this.item = item;
+            this.next = next;
+        }
+    }
+
+    private void addSeg (LineSegment seg) {
+        Node item;
+        item = new Node(seg, first);
+        first = item;
+        numOfSeg++;
+    }
+
     public BruteCollinearPoints(Point[] points)    // finds all line segments containing 4 points
     {
         if (points == null) throw new java.lang.NullPointerException();
@@ -18,23 +39,26 @@ public class BruteCollinearPoints {
         // this.points = points;
 
         //бежим в четверном цикле, перебирая все четверки
-        for (int i = 0; i < points.length; i++)
+        Comparator<Point> cp;
+        for (int i = 0; i < points.length; i++) {
+            cp = points[i].slopeOrder();
             for (int j = i + 1; j < points.length; j++)
                 for (int k = j + 1; k < points.length; k++)
                     for (int l = k + 1; l < points.length; l++) {
-                        if (points[i].slopeOrder().compare(points[j],points[k]) == 0
-                                &&  points[i].slopeOrder().compare(points[j],points[l]) == 0)
-                        {
-
+                        if (cp.compare(points[j], points[k]) == 0
+                                && cp.compare(points[j], points[l]) == 0) {
+                            addSeg(new LineSegment(points[i], points[l]));
                         }
 
                     }
+        }
 
     }
 
+
     public int numberOfSegments()        // the number of line segments
     {
-        return 0;
+        return numOfSeg;
     }
 
     public LineSegment[] segments()                // the line segments
